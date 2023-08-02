015128ccf5c56187d848b203ca4779670b0b94a5
Fabio Nascimento <fabionascimento@smartbr.com>
2023-08-02 13:13:32 -0300

    feat: config tailwind

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,17 +1,3 @@
-import HeroDefault from '@/components/HeroSections/HeroDefault/HeroDefault'
-import Header from '@/components/Headers/Header/Header'
-import Logo from '@/components/Layout/Logo/Logo'
-
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
 export default function Home() {
-  return (
-    <HeroDefault
-      header={<Header logo={<Logo />} navigation={navigation} fullwidth="no" />}
-    />
-  )
+  return <h1>Home</h1>
 }
